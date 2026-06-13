54d85e45d011d1a3466c4653cec23c17e328ce7d
jadhavsainathb <jadhavsainathb@gmail.com>
2026-06-13 16:34:06 +0530

    Model path Updated

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -6,7 +6,7 @@ import joblib
 
 # Download model from Hugging Face Hub
 model_path = hf_hub_download(
-    repo_id="jadhavsainath/tourism_package_prediction",
+    repo_id="jadhavsainath/churn-model",
     filename="best_churn_model_v1.joblib"
 )
 
